3860e05dc91c0114cd3593237b71d44413a65496
Isaac Muñoz Moreno <isaac@205-185-99-30.directcom.com>
2018-12-08 19:57:19 -0700

    strong

--- github.io/repository/homework/weather/js/functions.js
+++ github.io/repository/homework/weather/js/functions.js
@@ -1,27 +1,23 @@
-/* *************************************
-*  Weather Site JavaScript Functions
-************************************* */
+// Weather Site JavaScript Functions
+// console.log('My javascript is being read.');
 
-//Test values for feels like temp
+// Variables for Function Use
 // const temp = 31;
 // const speed = 5;
 // buildWC(speed, temp);
-
-//Test valuse for wind direction
-// const direction = "E";
+// const direction = "NW"; // Set your own value
 // windDial(direction);
+// let curCondition ="blizzard";
+// getCondition(curCondition);
+// let sumImage = getCondition(curCondition);
+// changeSummaryImage(sumImage);
 
-//Test values for background image
-// let currCondition = "sun";
-// let keywordA = getCondition(currCondition);
-// console.log(keywordA);
-// changeSummaryImage(keywordA);
-
-//Calculate wind chill
+// Calculate the Windchill
 function buildWC(speed, temp){
-    const feelTemp = document.getElementById("feels_temp");
+    const feelTemp = document.getElementById('feelTemp');
+
+    // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-    console.log(wc);
 
     // Round the answer down to integer
     wc = Math.floor(wc);
@@ -29,27 +25,28 @@ function buildWC(speed, temp){
     // If chill is greater than temp, return the temp
     wc = (wc > temp)?temp:wc;
 
-    //Display the wind chill
+    // Display the windchill
     console.log(wc);
-    feels_temp.innerHTML = "Feels Like: " + wc + "&deg;F";
+    feelTemp.innerHTML = wc;
 }
 
-//wind dial function
+//Wind Dial Function
 function windDial(direction){
-    //Get the wind dial container
+
+    // Get the wind dial container
     const dial = document.getElementById("dial");
-    console.log(direction);
-    //Determine the dial class
-    switch(direction){
+
+    // Determine the dial class
+    switch (direction){
         case "North":
-        case "N" :
-            dial.setAttribute("class", "n");
+        case "N":
+            dial.setAttribute("class", "n"); // "n" is the CSS rule selector
             break;
         case "NE":
         case "NNE":
-        case "ENE":
+        case "NEE":
             dial.setAttribute("class", "ne");
-            break;
+        break;
         case "NW":
         case "NNW":
         case "WNW":
@@ -62,7 +59,7 @@ function windDial(direction){
         case "SE":
         case "SSE":
         case "ESE":
-            dial.setAttribute("class","se");
+            dial.setAttribute("class", "se");
             break;
         case "SW":
         case "SSW":
@@ -75,90 +72,120 @@ function windDial(direction){
             break;
         case "West":
         case "W":
-             dial.setAttribute("class", "w");
-             break;
+            dial.setAttribute("class", "w");
+            break;
     }
 }
 
-//Function to get the current condition to set background image
-function getCondition(currentCondition){
-    currentCondition = currentCondition.toLowerCase();
-    var keyword = "";
-    //Determine keyword to be returned
-    if(currentCondition.includes("sun")||currentCondition.includes("clear")){
-        keyword = "clear";
-    }if(currentCondition.includes("cloud")||currentCondition.includes("overcast")){
-        keyword = "cloud";
-    }if(currentCondition.includes("fog")){
-        keyword = "fog";
-    }if(currentCondition.includes("rain")||currentCondition.includes("wet")||currentCondition.includes("thund")||currentCondition.includes("show")||currentCondition.includes("storm")){
-        keyword = "rain";
-    }if(currentCondition.includes("snow")||currentCondition.includes("flur")){
-        keyword = "snow";
-    }
-    console.log(keyword);
-    return keyword;
-
-}
-
-//Function to set summary image
-function changeSummaryImage(keywordReturn){
-    //Get the background element
-    const bgImg = document.getElementById("background");
-    console.log(keywordReturn);
-    //Determin image for background and header for summary
-    switch (keywordReturn){
+// The getCondition Function. This function changes the value of curCondition to a matching phrase.
+function getCondition(curCondition) {
+    // Let curWeather = document.getElementById("curWeather");
+    curCondition = curCondition.toLowerCase();
+    console.log(curCondition);
+    
+    switch (curCondition){
+        case "pleasant":
+        case "mostly clear":
         case "clear":
-            bgImg.setAttribute("class","clear");
-            document.getElementById("sum_image").setAttribute("src", "/weather/images/sunny-icon.png");
-            // document.getElementById("summary").textContent = "Clear";
-            break;
-        case "cloud":
-            bgImg.setAttribute("class","cloud");
-            document.getElementById("sum_image").setAttribute("src", "/weather/images/cloudy-icon.png");
-            // document.getElementById("summary").textContent = "Cloudy";
-            break;
+        case "sunny":
+        case "blue skies":
+        curCondition = "clear";
+        // curWeather.setAttribute("class", "clear");
+        break;
+        case "cloudy":
+        case "mostly cloudy":
+        case "clouds":
+        case "overcast":
+        case "partly cloudy":
+        curCondition = "clouds";
+        // curWeather.setAttribute("class", "clouds");
+        break;
         case "fog":
-            bgImg.setAttribute("class", "fog");
-            document.getElementById("sum_image").setAttribute("src", "/weather/images/fog-icon.png");
-            // document.getElementById("summary").textContent = "Foggy";
-            break;
+        case "mist":
+        case "misty":
+        case "foggy":
+        curCondition = "fog";
+        // curWeather.setAttribute("class", "fog");
+        break;
         case "rain":
-            bgImg.setAttribute("class", "rain");
-            document.getElementById("sum_image").setAttribute("src", "/weather/images/rain-icon.png");
-            // document.getElementById("summary").textContent = "Rainy";
-            break;
+        case "wet":
+        case "rainy":
+        case "humid":
+        case "drizzle":
+        case "pouring":
+        case "thunderstorm":
+        case "thunderstorms":
+        curCondition = "rain";
+        // curWeather.setAttribute("class", "rain");
+        break;
         case "snow":
-            bgImg.setAttribute("class", "snow");
-            document.getElementById("sum_image").setAttribute("src", "/weather/images/snow-icon.png");
-            // document.getElementById("summary").textContent = "Snow";
-            break;
+        case "snowstorm":
+        case "blizzard":
+        case "flurries":
+        case "snowy":
+        curCondition = "snow";
+        // curWeather.setAttribute("class", "snow");
+        break;
     }
+    console.log(curCondition);
+    return curCondition;
 }
-function getCode(LOCALE){
-    const API_KEY = 'orJNIxgg8idpickIj4R3RnUVG5JzGDYA';
-  const URL = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
-  fetch(URL)
-   .then(response => response.json())
-   .then(function (data) {
-        console.log('Json object from getCode function:');
-        console.log(data);
-        const locData = {}; // Create an empty object
-        locData['key'] = data.Key; // Add the value to the object
-        locData['name'] = data.LocalizedName;
-        locData['postal'] = data.PrimaryPostalCode;
-        locData['state'] = data.AdministrativeArea.LocalizedName;
-        locData['stateAbbr'] = data.AdministrativeArea.ID;
-        locData['geoposition'] = LOCALE;
-        locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
-        getWeather(locData);
-    })
-   .catch(error => console.log('There was a getCode error: ', error))
+// This function applies the image that matches the key phrase within one of the five weather categories.
+function changeSummaryImage(sumImage){
+    let curWeather = document.getElementById("curWeather");
+    sumImage = sumImage.toLowerCase()
+    console.log(sumImage);
+
+    switch (sumImage){
+        case "clear":
+        curWeather.setAttribute("class", "clear");
+        document.getElementById("clearImg").setAttribute("src", "/weather/images/optimized-imgs/clear-small.jpg");
+        break;
+        case "clouds":
+        curWeather.setAttribute("class", "clouds");
+        document.getElementById("clearImg").setAttribute("src", "/weather/images/optimized-imgs/clouds-small.jpg");
+        break;
+        case "fog":
+        curWeather.setAttribute("class", "fog");
+        document.getElementById("clearImg").setAttribute("src", "/weather/images/optimized-imgs/fog-small.jpg");
+        break;
+        case "rain":
+        curWeather.setAttribute("class", "rain");
+        document.getElementById("clearImg").setAttribute("src", "/weather/images/optimized-imgs/rain-small.jpg");
+        break;
+        case "snow":
+        curWeather.setAttribute("class", "snow");
+        document.getElementById("clearImg").setAttribute("src", "/weather/images/optimized-imgs/snow-small.jpg");
+        break;
+    }
+    console.log(sumImage);
 }
+
+// Get location code from API
+function getCode(LOCALE) {
+    const API_KEY = 'tocvaQZQ6L0G6iM4BtLXGZs53opoGq9T';
+    const URL = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
+    fetch(URL)
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getCode function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
+      })
+     .catch(error => console.log('There was a getCode error: ', error))
+  } // end getCode function
 
 // Get Current Weather data from API
 function getWeather(locData) {
-    const API_KEY = 'orJNIxgg8idpickIj4R3RnUVG5JzGDYA';
+    const API_KEY = 'tocvaQZQ6L0G6iM4BtLXGZs53opoGq9T';
     const CITY_CODE = locData['key']; // We're getting data out of the object
     const URL = "https://dataservice.accuweather.com/currentconditions/v1/"+CITY_CODE+"?apikey="+API_KEY+"&details=true";
     fetch(URL)
@@ -178,11 +205,11 @@ function getWeather(locData) {
       getHourly(locData); // Send data to getHourly function
       })
      .catch(error => console.log('There was an error: ', error))
-} // end getWeather function
+  } // end getWeather function
 
-  // Get next 12 hours of forecast data from API
+// Get next 12 hours of forecast data from API
 function getHourly(locData) {
-    const API_KEY = 'orJNIxgg8idpickIj4R3RnUVG5JzGDYA';
+    const API_KEY = 'tocvaQZQ6L0G6iM4BtLXGZs53opoGq9T';
     const CITY_CODE = locData['key'];
     const URL = "https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"+CITY_CODE+"?apikey="+API_KEY;
     fetch(URL)
@@ -227,86 +254,70 @@ function getHourly(locData) {
       buildPage(locData); // Send data to buildPage function
       })
       .catch(error => console.log('There was an error: ', error))
-} // end getHourly function
+  } // end getHourly function
 
-// formats a value into a 12h AM/PM time string
-function format_time(hour) {
-    if(hour > 23){
-      hour -= 24;
-    }
-    let amPM = (hour > 11) ? "pm" : "am";
-    if(hour > 12) {
-      hour -= 12;
-    } else if(hour == 0) {
-      hour = "12";
-    }
-    return hour + amPM;
-} // end format_time function
 
-//Setting of Hourly weather
-function setHourly(locData){
-   var curHour;
-
-   let hourlyReceived = [locData.hourTemp1,locData.hourTemp2,locData.hourTemp3,locData.hourTemp4,locData.hourTemp5,locData.hourTemp6,locData.hourTemp7,locData.hourTemp8,locData.hourTemp9,locData.hourTemp10,locData.hourTemp11,locData.hourTemp12];
-   curHour = locData.nextHour;
-   hourEl = 1;
-    console.log("nextHour within function");
-    console.log(curHour);
-    for(x=0;x<12;x++){
-        document.getElementById("hour"+hourEl).innerHTML= format_time(curHour) + ":";
-        document.getElementById("temp"+hourEl).innerHTML = hourlyReceived[x] + "&deg;F";
-        hourEl+=1;
-        curHour+=1;
-    }
-}
-// Populate the current location weather page
+  // Populate the current location weather page
 function buildPage(locData){
-    // Task 1 - Feed data to WC, Dial and Image functions
-    
-    // Task 2 - Populate location information
-    
-    // Task 3 - Populate weather information
-    
-    // Task 4 - Hide status and show main
-    
-
-    //Task 1
-    //Add Wind Chill
-    console.log("locData from buildPage");
-    console.log(locData);
+    // Task 1 - Feed AccuWeather data to WC, Dial and Image functions
+    // Take data from Wind Chill and feed it into the wind chill function
     let temp = locData.currentTemp;
     let speed = locData.windSpeed;
-    buildWC(speed,temp);
-    //Set wind dial
-    windDial(locData.windDirection);
-    //set image
+    buildWC(speed, temp);
+    // Feed AccuWeather data into Wind Dial function
+    windDial (locData.windDirection);
+    // Summary Image
     let sumImage = getCondition(locData.summary);
     console.log(sumImage);
     changeSummaryImage(sumImage);
 
-    //Task 2
-    document.getElementById("city").innerHTML = locData.name + ", " + locData.state;
+    // Task 2 - Populate location information
+    document.getElementById("locName").innerHTML = locData.name + ", " + locData.state;
     document.getElementById("zip").innerHTML = locData.postal;
-    document.getElementById("elevate").innerHTML = locData.elevation;
-    document.getElementById("lonlat").innerHTML = locData.geoposition;
+    document.getElementById("elevation").innerHTML = locData.elevation;
+    document.getElementById("geoLocation").innerHTML = locData.geoposition;
     document.title = locData.name + ", " + locData.stateAbbr + " | Weather Site";
 
-    //Task 3
-    //Set Current, high, low temp
-    document.getElementById("current_temp").innerHTML = locData.currentTemp+ "&deg;F";
-    document.getElementById("high_temp").innerHTML = locData.pastHigh + "&deg;F";
-    document.getElementById("low_temp").innerHTML = locData.pastLow + "&deg;F";
-    //Set wind speed, direction, gust
-    document.getElementById("wind_speed").innerHTML = locData.windSpeed + "mph";
-    document.getElementById("gust").innerHTML = "Gusts: " + locData.windGust +"mph";
-    document.getElementById("direct").innerHTML = "Direction: " + locData.windDirection;
-    //Set summary section of current weather
-    setHourly(locData);
-    document.getElementById("summary").innerHTML = locData.summary;
-    console.log("locData object item");
-    console.log(locData.elevation);
+    // Task 3 - Populate weather information
+    document.getElementById("currentTempValue").innerHTML = locData.currentTemp;
+    document.getElementById("highTempValue").innerHTML = locData.pastHigh;
+    document.getElementById("lowTempValue").innerHTML = locData.pastLow;
 
-    //Task 4
-    document.getElementById("main-cont").setAttribute("class", "show");
+    document.getElementById("windDirection").innerHTML = locData.windDirection;
+    document.getElementById("windSpeedValue").innerHTML = locData.windSpeed;
+    document.getElementById("gustValue").innerHTML = locData.windGust;
+
+    document.getElementById("rain-title").innerHTML = locData.summary;
+    console.log(locData.summary);
+
+    // Task 4 - Hide status and show main
+    document.getElementById("page-main").setAttribute("class", "show");
     document.getElementById("status").setAttribute("class", "hide");
-}
+    // For the search.html page
+    document.getElementById("searchResults").setAttribute("class", "hide");
+    }
+
+    // Get location info, based on city key, from API
+function getLocationByKey(cityKey) {
+    const API_KEY = 'tocvaQZQ6L0G6iM4BtLXGZs53opoGq9T';
+    const URL = 'https://dataservice.accuweather.com/locations/v1/'+cityKey+'?apikey='+API_KEY;
+    fetch(URL)
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getLocationByKey function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      let lat = data.GeoPosition.Latitude;
+      let long = data.GeoPosition.Longitude;
+      const LOCALE = lat+', '+long;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
+      })
+     .catch(error => console.log('There was a getLocationByKey error: ', error))
+    } // end getLocationByKey function
